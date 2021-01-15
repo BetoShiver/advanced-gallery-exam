b9ad432c7d8ce4ae229d6e6e651490c28909eb51
BetoShiver <69962093+BetoShiver@users.noreply.github.com>
2021-01-15 13:03:49 +0200

    rotate button working

--- src/components/Image/Image.js
+++ src/components/Image/Image.js
@@ -1,30 +1,52 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import FontAwesome from 'react-fontawesome';
-import './Image.scss';
+import React from "react";
+import PropTypes from "prop-types";
+import FontAwesome from "react-fontawesome";
+import "./Image.scss";
 
 class Image extends React.Component {
   static propTypes = {
     dto: PropTypes.object,
-    galleryWidth: PropTypes.number
+    galleryWidth: PropTypes.number,
   };
 
   constructor(props) {
     super(props);
     this.calcImageSize = this.calcImageSize.bind(this);
     this.state = {
-      size: 200
+      size: 200,
+      currentDegrees: 0,
+      btnDegrees: 360
     };
+    this.rotateImg = this.rotateImg.bind(this);
   }
 
   calcImageSize() {
-    const {galleryWidth} = this.props;
+    const { galleryWidth } = this.props;
     const targetSize = 200;
     const imagesPerRow = Math.round(galleryWidth / targetSize);
-    const size = (galleryWidth / imagesPerRow);
+    const size = galleryWidth / imagesPerRow;
     this.setState({
-      size
+      size,
     });
+  }
+
+  handleRotate() {
+    let newDegrees = this.state.currentDegrees + 90;
+    if (newDegrees >= 360) {
+      newDegrees -= newDegrees;
+    }
+    this.setState({
+      currentDegrees: newDegrees,
+      btnDegrees: 360 - newDegrees,
+    });
+  }
+
+  handleDelete() {
+    console.log("rotate clicked");
+  }
+
+  handleExpand() {
+    console.log("rotate clicked");
   }
 
   componentDidMount() {
@@ -40,15 +62,21 @@ class Image extends React.Component {
       <div
         className="image-root"
         style={{
+          transform: `rotate(${this.state.currentDegrees}deg)`,
           backgroundImage: `url(${this.urlFromDto(this.props.dto)})`,
-          width: this.state.size + 'px',
-          height: this.state.size + 'px'
+          width: this.state.size + "px",
+          height: this.state.size + "px",
         }}
-        >
-        <div>
-          <FontAwesome className="image-icon" name="sync-alt" title="rotate"/>
-          <FontAwesome className="image-icon" name="trash-alt" title="delete"/>
-          <FontAwesome className="image-icon" name="expand" title="expand"/>
+      >
+        <div style={{ transform: `rotate(${this.state.btnDegrees}deg)` }}>
+          <FontAwesome
+            onClick={() => this.handleRotate()}
+            className="image-icon"
+            name="sync-alt"
+            title="rotate"
+          />
+          <FontAwesome className="image-icon" name="trash-alt" title="delete" />
+          <FontAwesome className="image-icon" name="expand" title="expand" />
         </div>
       </div>
     );
